0e72163d03f5c7022973562ca37a23ac1944cb01
Vaughany <paulieboo@gmail.com>
2014-07-08 17:33:05 +0100

    Removed some commented-out code.

--- define.class.php
+++ define.class.php
@@ -35,13 +35,6 @@ class profile_define_maincourse extends profile_define_base {
      * @param moodleform $form
      */
     public function define_form_specific($form) {
-        // Param 1 for menu type contains the options.
-        //$form->addElement('textarea', 'param1', get_string('profilemenuoptions', 'admin'), array('rows' => 6, 'cols' => 40));
-        //$form->setType('param1', PARAM_TEXT);
-
-        // Default data.
-        //$form->addElement('text', 'defaultdata', get_string('profiledefaultdata', 'admin'), 'size="50"');
-        //$form->setType('defaultdata', PARAM_TEXT);
     }
 
     /**
@@ -52,21 +45,7 @@ class profile_define_maincourse extends profile_define_base {
      * @return array
      */
     public function define_validate_specific($data, $files) {
-        $err = array();
-/*
-        $data->param1 = str_replace("\r", '', $data->param1);
-
-        // Check that we have at least 2 options.
-        if (($options = explode("\n", $data->param1)) === false) {
-            $err['param1'] = get_string('profilemenunooptions', 'admin');
-        } else if (count($options) < 2) {
-            $err['param1'] = get_string('profilemenutoofewoptions', 'admin');
-        } else if (!empty($data->defaultdata) and !in_array($data->defaultdata, $options)) {
-            // Check the default data exists in the options.
-            $err['defaultdata'] = get_string('profilemenudefaultnotinoptions', 'admin');
-        }
-*/
-        return $err;
+        return array();
     }
 
     /**
